344912171c90f37916f3685b1009fdb2a378029f
psyhedeliq <filipcornelcristian@gmail.com>
2019-07-26 20:50:27 +0100

    map method

--- functional-programming.js
+++ functional-programming.js
@@ -8,15 +8,29 @@ const animals = [
 ];
 // console.log(animals);
 
-//********** filter **********//
+//******************** filter ********************//
 const dogs = animals.filter(animal => animal.species === "dog");
 // console.log(dogs);
 
-//********** filter (the old way) **********//
-var dogs1 = [];
+//******************** filter (the old way) ********************//
+/*
+var dogs = [];
 for (let i = 0; i < animals.length; i++) {
   if (animals[i].species === "fish") {
-    dogs1.push(animals[i]);
+    dogs.push(animals[i]);
   }
 }
-// console.log(dogs1);
+console.log(dogs);
+*/
+
+//******************** map ********************//
+const names = animals.map(animal => animal.name);
+console.log(names);
+//******************** map (the old way) ********************//
+/*
+const names = [];
+for (let i = 0; i < animals.length; i++) {
+  names.push(animals[i].name);
+}
+console.log(names);
+*/
