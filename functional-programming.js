13d267f7ed4d1953fa29e799f113048c04016b6a
psyhedeliq <filipcornelcristian@gmail.com>
2019-07-28 14:18:19 +0100

    recursion

--- functional-programming.js
+++ functional-programming.js
@@ -96,4 +96,50 @@ let dragon = (name, size, element) => {
 //******************** currying version ********************//
 let dragon1 = name => size => element =>
   name + " is a " + size + " dragon that breathes " + element + "!";
-console.log(dragon1("fluffykins")("tiny")("lightning"));
+// console.log(dragon1("fluffykins")("tiny")("lightning"));
+
+//******************** recursion ********************//
+let countDownFrom = num => {
+  if (num === 0) return;
+  console.log(num);
+  countDownFrom(num - 1);
+};
+// countDownFrom(10);
+
+let categories = [
+  { id: "animals", parent: null },
+  { id: "mammals", parent: "animals" },
+  { id: "cats", parent: "mammals" },
+  { id: "dogs", parent: "mammals" },
+  { id: "chihuahua", parent: "dogs" },
+  { id: "labrador", parent: "dogs" },
+  { id: "persian", parent: "cats" },
+  { id: "siamese", parent: "cats" }
+];
+
+let makeTree = (categories, parent) => {
+  let node = {};
+  categories
+    .filter(c => c.parent === parent)
+    .forEach(c => (node[c.id] = makeTree(categories, c.id)));
+
+  return node;
+};
+// console.log(JSON.stringify(makeTree(categories, null), null, 2));
+
+// Expected result
+/*
+{
+  animals: {
+    mammals: {
+      dogs: {
+        chihuahua: null
+        labrador: null
+      },
+      cats: {
+        persian: null
+        siamese: null
+      }
+    }
+  }
+}*/
